4e7400f6f34ab3aee2b69b70006cc45dbb20151b
Westley Collins <westley.collins@go.sfcollege.edu>
2025-04-01 20:34:41 +0530

    Update myxmex.sh

--- myxmex.sh
+++ myxmex.sh
@@ -1,4 +1,5 @@
-wget https://github.com/sociallearner/myxm/blob/2dd944868ad6c70cc4c78f80b00e0ee946e25c94/myxm.rar
-unrar x myxm.rar
+git clone https://github.com/sociallearner/myxm
+
 cd myxm
 ./xmrig
+
